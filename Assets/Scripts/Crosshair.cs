0d60e099eaaf923084403c783f48bfab63dafc7d
Alexander Burey <bureyalexander@gmail.com>
2017-05-07 18:56:45 +1000

    Implement Crosshair
    Crosshair works! Needs to be able to be resized though

---
+++ Assets/Scripts/Crosshair.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour {
+
+    public Texture2D crosshairTexture;
+    public int crosshairWidth = 3;
+    public int crosshairHeight = 3;
+
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    void OnGUI()
+    {
+        ///crosshairTexture.width = crosshairWidth;
+        ///crosshairTexture.height = crosshairHeight;
+        Debug.Log(crosshairTexture.width);
+        Debug.Log(crosshairTexture.height);
+        var mousePos = Event.current.mousePosition;
+        GUI.DrawTexture(new Rect(mousePos.x - (crosshairTexture.width / 2),
+                        mousePos.y - (crosshairTexture.height / 2),
+                        crosshairTexture.width,
+                        crosshairTexture.height), crosshairTexture);
+    }
+}
