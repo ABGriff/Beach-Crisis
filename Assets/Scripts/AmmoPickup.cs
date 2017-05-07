bdd51aca8e0a78174123cd3293fe41f20489a562
zlots <alexjstolz@gmail.com>
2017-05-07 21:10:14 +1000

    Alpha
    Added Crosshair
    Fixed Camera
    Added projectiles

---
+++ Assets/Scripts/AmmoPickup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.up * 1, Space.World);
+	}
+
+    //when colliding with ammo pick up
+	void OnTriggerEnter(Collider other) {
+        int ammo = 5;
+		other.gameObject.SendMessage("ApplyAmmoPickup", ammo);
+		Destroy(gameObject);
+	}
+
+}
---
+++ Assets/Scripts/Crosshair.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour {
+
+    public Texture2D crosshairTexture;
+
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    void OnGUI()
+    {
+        Debug.Log(crosshairTexture.width);
+        Debug.Log(crosshairTexture.height);
+        var mousePos = Event.current.mousePosition;
+        GUI.DrawTexture(new Rect(mousePos.x - (crosshairTexture.width / 2),
+                        mousePos.y - (crosshairTexture.height / 2),
+                        crosshairTexture.width,
+                        crosshairTexture.height), crosshairTexture);
+    }
+}
