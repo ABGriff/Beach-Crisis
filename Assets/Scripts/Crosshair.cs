ecff39a3a2c0d89e34992324ae27cd6e6842af43
Alexander Burey <bureyalexander@gmail.com>
2017-05-08 00:29:06 +1000

    Update Shooting
    Shooting is now 'mostly' aimed at the cursor and I've added a gun to
    shoot the bullets from.
    BUG: Gun is stuck on an interesting angle. hmmm

--- Assets/Scripts/Crosshair.cs
+++ Assets/Scripts/Crosshair.cs
@@ -2,45 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Crosshair : MonoBehaviour {
+public class Crosshair : MonoBehaviour
+{
 
     public Texture2D crosshairTexture;
-<<<<<<< HEAD
-=======
-    public int crosshairWidth = 3;
-    public int crosshairHeight = 3;
-<<<<<<< HEAD
->>>>>>> origin/master
-=======
->>>>>>> 0d60e099eaaf923084403c783f48bfab63dafc7d
->>>>>>> origin/master
 
     void Start()
     {
         Cursor.visible = false;
-<<<<<<< HEAD
         Cursor.lockState = CursorLockMode.Confined;
-=======
-<<<<<<< HEAD
->>>>>>> origin/master
-=======
->>>>>>> 0d60e099eaaf923084403c783f48bfab63dafc7d
->>>>>>> origin/master
     }
 
     void OnGUI()
     {
-<<<<<<< HEAD
-=======
-        ///crosshairTexture.width = crosshairWidth;
-        ///crosshairTexture.height = crosshairHeight;
-<<<<<<< HEAD
->>>>>>> origin/master
-=======
->>>>>>> 0d60e099eaaf923084403c783f48bfab63dafc7d
->>>>>>> origin/master
-        Debug.Log(crosshairTexture.width);
-        Debug.Log(crosshairTexture.height);
         var mousePos = Event.current.mousePosition;
         GUI.DrawTexture(new Rect(mousePos.x - (crosshairTexture.width / 2),
                         mousePos.y - (crosshairTexture.height / 2),
---
+++ Assets/Scripts/GunController.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+var speed = float; //Rotation speed of gun
+var zdepth: float;
+
+
+function Update() {
+    var x = Input.mousePosition.x;
+    var y = Input.mousePosition.y;
+    var mousePosition = Camera.main.ScreenToWorldPoint(Vector3(x, y, zdepth));
+    var rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
+
+    transform.rotation = rot;
+
+
+
+
+   //var x = Input.GetAxis("Mouse X") * Time.deltaTime * speed; //controls horizontal movement of gun
+   //var y = Input.GetAxis("Mouse Y") * Time.deltaTime * speed; //controls vertical movement of gun
+   // transform.Rotate(-y, x, 0);
+}
